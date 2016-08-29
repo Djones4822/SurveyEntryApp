0eec403077e5a400b1c3435a710009b2538a925a
Dave Jones <david.jones4822@gmail.com>
2016-08-29 11:53:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(
-        name='REACH Survey Entry',
+        name='Survey Entry',
         version='3.5',
         url='',
         license='MIT',
         author='David Jones',
         author_email='davidj@gsfc.org',
-        description='GUI For entering survey data from REACH surveys',
-        executables= [Executable("G:\REACH\REACH_Data_Entry\REACH Survey Entry.py", base=base)],
+        description='GUI For entering survey data from surveys',
+        executables= [Executable(".\Survey Entry.py", base=base)],
         options={"build_exe":{"packages":['tkinter','cx_Oracle','datetime','time','enter_survey','student_lookup',
                                           'queryfuncs','login','gui', 'datetime', 'add_respondent', 'possible_matches',
                                           'fuzzywuzzy', 'Levenshtein']}}
